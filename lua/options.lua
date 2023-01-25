0a764a3d990aa80edd6d83e3b0b0899b7b1d7a1e
jhs <rurioil@naver.com>
2023-01-25 21:39:38 +0900

    init nvim lua

---
+++ lua/options.lua
@@ -0,0 +1,18 @@
+local set = vim.opt
+
+set.number = true
+set.relativenumber = true
+
+vim.scriptencoding = 'utf-8'
+set.encoding = 'utf-8'
+set.fileencoding = 'utf-8'
+
+set.scrolloff = 5
+set.sidescrolloff = 3
+
+set.hlsearch = true
+set.incsearch = true
+
+set.mouse = 'a'
+set.clipboard:append('unnamedplus')
+
---
+++ lua/plugins.lua
@@ -0,0 +1,16 @@
+
+return require('packer').startup(function(use)
+  use({
+    "folke/noice.nvim",
+    requires = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    }
+  })
+  use 'wbthomason/packer.nvim'
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  }
+end)
