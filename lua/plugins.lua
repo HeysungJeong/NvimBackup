0fa15ed78ce5c35822e0929f8143506681bc6983
jhs <rurioil@naver.com>
2023-01-27 14:11:11 +0900

    to day test

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,16 +1,5 @@
 
 return require('packer').startup(function(use)
-  use({
-    "folke/noice.nvim",
-    requires = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    }
-  })
   use 'wbthomason/packer.nvim'
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  }
+  use "neovim/nvim-lspconfig"
 end)
