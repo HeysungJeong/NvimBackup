b0c24b7ba5d9c15ce2ce425eeba61fcbc9709615
jhs <rurioil@naver.com>
2023-01-31 21:35:52 +0900

    944

--- lua/pLsp.lua
+++ lua/pLsp.lua
@@ -1,12 +1,13 @@
 local on_attach = function(client, bufnr)
   -- format on save
-  vim.api.nvim_create_autocmd('BufWritePre', {
-    group = vim.api.nvim_create_augroup('LspFormatting', { clear = true }),
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.buf.format()
-    end
-  })
+--  vim.api.nvim_create_autocmd('BufWritePre', {
+--    group = vim.api.nvim_create_augroup('LspFormatting', { clear = true }),
+--    buffer = bufnr,
+--    callback = function()
+--      vim.lsp.buf.format()
+--    end
+--  })
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 ---@diagnostic disable-next-line: undefined-global
 local capabilities = vim.lsp.protocol.make_client_capabilities()
